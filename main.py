9fb8e1071e2f2f91b02103a82d846f1cbc9347f5
jinchuan <1767083617@qq.com>
2022-05-08 18:24:05 +0800

    demo

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ from scene import Scene
 import taichi as ti
 from taichi.math import *
 
-scene = Scene(exposure=10)
+scene = Scene(voxel_edges = 0, exposure=1)
 scene.set_floor(-0.05, (1.0, 1.0, 1.0))
-scene.set_background_color((1.0, 0, 0))
+scene.set_background_color((0.5, 0.5, 0.4))
+scene.set_directional_light((1, 1, -1), 0.2, (1, 0.8, 0.6))
+
+
+@ti.func
+def create_cylinder(pos, radius, height, color):
+    for I in ti.grouped(
+            ti.ndrange((pos[0] - radius, pos[0] + radius), (pos[1], pos[1] + height),
+                       (pos[2] - radius, pos[2] + radius))):
+        if I[0] ** 2 + I[2] ** 2 <= radius ** 2: 
+            scene.set_voxel(I, 1, color)
 
 
 @ti.kernel
 def initialize_voxels():
     # Your code here! :-)
-    scene.set_voxel(vec3(0, 0, 0), 2, vec3(0.9, 0.1, 0.1))
+    create_cylinder(ivec3(0, 0, 0), 30, 5, vec3(0.9, 0.1, 0.1))
 
 
 initialize_voxels()
