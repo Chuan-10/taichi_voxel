015cd313fb59fbdd5d856c8dd83a223ae2a80df6
jinchuan <1767083617@qq.com>
2022-05-11 11:07:06 +0800

    v1.0

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from scene import Scene
 import taichi as ti
 from taichi.math import *
 
-scene = Scene(voxel_edges = 0, exposure=1.5)
-scene.set_floor(-1.0, (1.0, 1.0, 1.0))
+scene = Scene(voxel_edges = 0, exposure=1.7)
+scene.set_floor(-30.0, (0/255.0, 100/255.0, 0/255.0))
 scene.set_background_color((135/255.0, 216/255.0, 235/255.0))
 scene.set_directional_light((1, 1, -1), 0.2, (1, 0.8, 0.6))
 
